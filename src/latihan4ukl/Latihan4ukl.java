db46242882c2fffb8cb53f7a29bac22d47732f3a
ZAID6470 <muhammad_zaidan_29rpl@student.smktellkom-mlg.sch.id>
2021-08-03 14:17:22 +0700

    UKL

---
+++ src/latihan4ukl/Latihan4ukl.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan4ukl;
+
+/**
+ *
+ * @author MOKLET-2
+ */
+public class Latihan4ukl {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 4;
+        int d = 5;
+        int e = 5;
+        int f;
+
+    for (int b = 1; b <= a; ++b) {
+      for (int c = 1; c <= b; ++c) {
+          
+          f = d;
+          d += e;
+          
+        System.out.print(" "+f);
+      }
+      System.out.println("");
+    }
+    }
+    
+}
---
+++ src/latihan4ukl2/latihan4ukl2.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan4ukl2;
+
+/**
+ *
+ * @author MOKLET-2
+ */
+public class latihan4ukl2 {
+    public static void main(String[] args) {
+        int a = 5;
+        int d = 5;
+        int e = 5;
+        int f;
+    
+    for (int b = 1; b <= a; ++b) { 
+        System.out.println("");
+      
+        for (int c = b; c <= a; ++c) {
+            
+            f = d;
+            d += e;
+                   
+            System.out.print(" "+f);
+      }
+      
+    }
+    
+    }
+}
